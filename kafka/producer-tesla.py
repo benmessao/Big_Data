aa5b31e88adeacb4cf860244a08ed76aefb9cc6a
chleo <chevrierch@eisti.eu>
2023-01-13 16:23:44 +0100

    test producer

--- kafka/producer-tesla.py
+++ kafka/producer-tesla.py
@@ -9,14 +9,23 @@ import time
 
 # Initialize configurations from "python.config" file
 CONF = ccloud_lib.read_ccloud_config("python.config")
-TOPIC = "my_first_topic" 
+TOPIC = "nasdaq_prices"
 
 # Create Producer instance
 producer_conf = ccloud_lib.pop_schema_registry_params_from_config(CONF)
+producer_topic = ccloud_lib.create_topic(CONF, TOPIC)
 producer = Producer(producer_conf)
 
-# Create topic if it doesn't already exist
-ccloud_lib.create_topic(CONF, TOPIC)
+import requests
+
+url = "https://realstonks.p.rapidapi.com/TSLA"
+
+headers = {
+	"X-RapidAPI-Key": "dc9665504emshd4b02932d40379bp1b60dcjsn1177cf22dd2f",
+	"X-RapidAPI-Host": "realstonks.p.rapidapi.com"
+}
+
+#print(data["price"])
 
 def acked(err, msg):
     global delivered_records
@@ -31,12 +40,20 @@ def acked(err, msg):
 try:
     # Starts an infinite while loop that produces random current temperatures
     while True:
-        record_key = "weather"
+
+        response = requests.request("GET", url, headers=headers)
+
+        data = json.loads(response.text)
+
+        record_key = "tesla_price"
+        
         record_value = json.dumps(
             {
-                "degrees_in_celsion": np.random.randint(10, 40)
+                "time_of_price": response.headers.get("Date"),
+                "current_price": data["price"] 
             }
         )
+        
         print("Producing record: {}\t{}".format(record_key, record_value))
 
         # This will actually send data to your topic
@@ -44,10 +61,10 @@ try:
             TOPIC,
             key=record_key,
             value=record_value,
-            on_delivery=acked
+            #on_delivery=acked
         )
         producer.poll(0)
-        time.sleep(0.5)
+        time.sleep(1)
         
 
  # Interrupt infinite loop when hitting CTRL+C
