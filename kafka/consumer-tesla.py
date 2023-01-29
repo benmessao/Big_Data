80a47e09b4a20865fe86bc6765bf971f96338e57
chleo <chevrierch@eisti.eu>
2023-01-29 16:25:31 +0100

    kafka td partie 2 consumer tasla

---
+++ kafka/consumer-tesla.py
@@ -0,0 +1,62 @@
+# Example written based on the official 
+# Confluent Kakfa Get started guide https://github.com/confluentinc/examples/blob/7.1.1-post/clients/cloud/python/consumer.py
+
+from confluent_kafka import Consumer
+import json
+import ccloud_lib
+import time
+import pandas as pd
+
+# Initialize configurations from "python.config" file
+CONF = ccloud_lib.read_ccloud_config("python.config")
+TOPIC = "nasdaq_prices" 
+
+# Create Consumer instance
+# 'auto.offset.reset=earliest' to start reading from the beginning of the
+# topic if no committed offsets exist
+consumer_conf = ccloud_lib.pop_schema_registry_params_from_config(CONF)
+consumer_conf['group.id'] = 'tesla_consumer'
+consumer_conf['auto.offset.reset'] = 'earliest' # This means that you will consume latest messages that your script haven't consumed yet!
+consumer = Consumer(consumer_conf)
+
+# Subscribe to topic
+consumer.subscribe([TOPIC])
+
+#creat the dataframe
+df = pd.DataFrame(data=[], columns=["time_of_price", "current_price"])
+print("dataframe au début",df)
+# Process messages
+try:
+    messages_list=[]
+    while True:
+        msg = consumer.poll(1.0) # Search for all non-consumed events. It times out after 1 second
+        if msg is None:
+            # No message available within timeout.
+            # Initial message consumption may take up to
+            # `session.timeout.ms` for the consumer group to
+            # rebalance and start consuming
+            print("Waiting for message or event/error in poll()")
+            continue
+        elif msg.error():
+            print('error: {}'.format(msg.error()))
+        else:
+            # Check for Kafka message
+            record_key = msg.key()
+            record_value = msg.value()
+            data = json.loads(record_value)
+            price = data["current_price"] #get the curent price
+            date = data["time_of_price"]
+            #print(f"The current price is {price} for a tesla")
+            messages_list.append([date, price])
+            if (len(messages_list)==10):
+                print("Mise à jour du dataframe avec les 10 derniers messages")
+                messages_df = pd.DataFrame(data=messages_list, columns=["time_of_price", "current_price"])
+                messages_list=[]
+                df = df.append(messages_df, ignore_index=True)
+
+            time.sleep(1) # Wait half a second
+except KeyboardInterrupt:
+    print("dataframe à la fin",df)
+finally:
+    # Leave group and commit final offsets
+    consumer.close()
